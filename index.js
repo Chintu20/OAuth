4043f44359271937eaa247c9cef0ede6753c962e
chinto20 <vkarthikeyan21@gmail.com>
2018-02-23 08:48:17 +0530

    updated clientsecret

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keyconfig = require('./config/keys');
-/*
-message to Stephen:
-I have removed the ClientID and ClientSecret from the keys.js file 
 
-**It is just a sample application nothing is secret, just for practice I removed
- */
 
 const PORT = process.env.port || 5050;
 const app = express();
@@ -15,7 +10,7 @@ const app = express();
 passport.use(
     new GoogleStrategy
     (
-            {clientID:keyconfig.googleclientID,clientSecret:keyconfig.googleclientID,callbackURL: '/auth/google/callback'},
+            {clientID:keyconfig.googleclientID,clientSecret:keyconfig.googleSecret,callbackURL: '/auth/google/callback'},
             (profile) => {console.log('profile : ', profile);}
     )
     );
