0fd4015275c100ba59268d22c66e4eed2c971424
chinto20 <vkarthikeyan21@gmail.com>
2018-02-23 08:28:13 +0530

    updated key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleclientID : 'http://867086455919-ood6efqs9k1r177oj4gb1a10kcunbtq2.apps.googleusercontent.com',
+    googleclientID : '867086455919-ood6efqs9k1r177oj4gb1a10kcunbtq2.apps.googleusercontent.com',
     googleSecret : 'Yxzdhdxrc7-c6cXL17R9DVfg'
 }
